96ab623c7f00ec5f1fee844105df3fce215f537b
Olegechkin <olechka5.14@yandex.ru>
2023-05-01 20:02:34 +0400

    Цепочка зависимостей

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
+    private Duck5 dusk;
+
+    @Autowired
+    public Rabbit4(Duck5 dusk){
+        this.dusk = dusk;
+    }
+
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + dusk.toString();
     }
 }
